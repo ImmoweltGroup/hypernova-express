4ed1e1ae433f279c2cf032bc38f0844598c3ecc1
Tyll Weiß <tyll.weiss.extern@axelspringer.com>
2017-09-06 17:44:57 +0200

    TASK: Update to the new code-style guidelines of Immowelt

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@
 import type {$Response, $Request} from 'express';
 
 type OptsType = {
-	renderer: {
-		render: (query: Object) => Promise<string>
-	},
-	createRequestProps: (req: $Request) => Promise<Object>,
-	templatePath: string,
-	templateMarker?: string
+  renderer: {
+    render: (query: Object) => Promise<string>
+  },
+  createRequestProps: (req: $Request) => Promise<Object>,
+  templatePath: string,
+  templateMarker?: string
 };
 
 const pify = require('pify');
@@ -16,7 +16,7 @@ const fs = require('fs');
 
 const readFile = pify(fs.readFile);
 const err = (msg: string) => {
-	throw new Error(`createHypernovaMiddleware(): ${msg}`);
+  throw new Error(`createHypernovaMiddleware(): ${msg}`);
 };
 
 /**
@@ -26,43 +26,52 @@ const err = (msg: string) => {
  * @return {Function}      The configured express middleware.
  */
 function createHypernovaMiddleware(opts: OptsType) {
-	const {
-		renderer,
-		createRequestProps,
-		templatePath,
-		templateMarker = '<i data-html></i>'
-	} = (opts || {});
+  const {
+    renderer,
+    createRequestProps,
+    templatePath,
+    templateMarker = '<i data-html></i>'
+  } =
+    opts || {};
 
-	if (!renderer || typeof renderer !== 'object' || typeof renderer.render !== 'function') {
-		err(`Option "renderer" must be an instance of the hypernova-client Class.`);
-	}
+  if (
+    !renderer ||
+    typeof renderer !== 'object' ||
+    typeof renderer.render !== 'function'
+  ) {
+    err(`Option "renderer" must be an instance of the hypernova-client Class.`);
+  }
 
-	if (typeof templatePath !== 'string') {
-		err(`Option "templatePath" must be a string pointing to the HTML template to use when creating the finalized HTML.`);
-	}
+  if (typeof templatePath !== 'string') {
+    err(
+      `Option "templatePath" must be a string pointing to the HTML template to use when creating the finalized HTML.`
+    );
+  }
 
-	if (typeof createRequestProps !== 'function') {
-		err(`Option "createRequestProps" must be a function that resolves with the renderer props/query.`);
-	}
+  if (typeof createRequestProps !== 'function') {
+    err(
+      `Option "createRequestProps" must be a function that resolves with the renderer props/query.`
+    );
+  }
 
-	return (req: $Request, res: $Response) => {
-		createRequestProps(req)
-			.then(props => {
-				return Promise.all([
-					readFile(templatePath, 'utf8'),
-					renderer.render(props)
-				]);
-			})
-			.then(([templateHtml, uiAppHtml]: Array<string>) => {
-				const html = templateHtml.replace(templateMarker, uiAppHtml);
+  return (req: $Request, res: $Response) => {
+    createRequestProps(req)
+      .then(props => {
+        return Promise.all([
+          readFile(templatePath, 'utf8'),
+          renderer.render(props)
+        ]);
+      })
+      .then(([templateHtml, uiAppHtml]: Array<string>) => {
+        const html = templateHtml.replace(templateMarker, uiAppHtml);
 
-				res.send(html);
-				res.end();
+        res.send(html);
+        res.end();
 
-				return null;
-			})
-			.catch(console.error);
-	};
+        return null;
+      })
+      .catch(console.error);
+  };
 }
 
 module.exports = createHypernovaMiddleware;
--- src/index.spec.js
+++ src/index.spec.js
@@ -5,74 +5,77 @@ const express = require('express');
 const createHypernovaMiddleware = require('./index.js');
 
 describe('createHypernovaMiddleware()', () => {
-	beforeEach(() => {
-		mockFs({
-			'/foo/bar/index.html': '<html><i data-html></i></html>'
-		}, {
-			createCwd: false,
-			createTmp: false
-		});
-	});
+  beforeEach(() => {
+    mockFs(
+      {
+        '/foo/bar/index.html': '<html><i data-html></i></html>'
+      },
+      {
+        createCwd: false,
+        createTmp: false
+      }
+    );
+  });
 
-	afterEach(() => {
-		mockFs.restore();
-	});
+  afterEach(() => {
+    mockFs.restore();
+  });
 
-	it('should be a function.', () => {
-		expect(typeof createHypernovaMiddleware).toBe('function');
-	});
+  it('should be a function.', () => {
+    expect(typeof createHypernovaMiddleware).toBe('function');
+  });
 
-	it('should throw an error if no "renderer" option was passed.', () => {
-		expect(() => createHypernovaMiddleware({})).toThrow();
-	});
+  it('should throw an error if no "renderer" option was passed.', () => {
+    expect(() => createHypernovaMiddleware({})).toThrow();
+  });
 
-	it('should throw an error if no "templatePath" option was passed.', () => {
-		const renderer = {render: () => Promise.resolve()};
+  it('should throw an error if no "templatePath" option was passed.', () => {
+    const renderer = {render: () => Promise.resolve()};
 
-		expect(() => createHypernovaMiddleware({renderer})).toThrow();
-	});
+    expect(() => createHypernovaMiddleware({renderer})).toThrow();
+  });
 
-	it('should throw an error if no "createRequestProps" option was passed.', () => {
-		const renderer = {render: () => Promise.resolve()};
-		const templatePath = '/foo/bar.html';
+  it('should throw an error if no "createRequestProps" option was passed.', () => {
+    const renderer = {render: () => Promise.resolve()};
+    const templatePath = '/foo/bar.html';
 
-		expect(() => createHypernovaMiddleware({renderer, templatePath})).toThrow();
-	});
+    expect(() => createHypernovaMiddleware({renderer, templatePath})).toThrow();
+  });
 
-	it('should return a middleware function when called.', () => {
-		const middleware = createHypernovaMiddleware({
-			renderer: {
-				render: () => Promise.resolve()
-			},
-			createRequestProps: () => Promise.resolve({}),
-			templatePath: '/foo/bar/index.html'
-		});
+  it('should return a middleware function when called.', () => {
+    const middleware = createHypernovaMiddleware({
+      renderer: {
+        render: () => Promise.resolve()
+      },
+      createRequestProps: () => Promise.resolve({}),
+      templatePath: '/foo/bar/index.html'
+    });
 
-		expect(typeof middleware).toBe('function');
-	});
+    expect(typeof middleware).toBe('function');
+  });
 
-	it('should call the renderer.render method and provide it with the requestProps from the createRequestProps option.', done => {
-		const html = '<div>foo</div>';
-		const requestProps = {};
-		const options = {
-			renderer: {
-				render: sinon.spy(() => Promise.resolve(html))
-			},
-			createRequestProps: sinon.spy(() => Promise.resolve(requestProps)),
-			templatePath: '/foo/bar/index.html'
-		};
-		const app = express();
+  it('should call the renderer.render method and provide it with the requestProps from the createRequestProps option.', done => {
+    const html = '<div>foo</div>';
+    const requestProps = {};
+    const options = {
+      renderer: {
+        render: sinon.spy(() => Promise.resolve(html))
+      },
+      createRequestProps: sinon.spy(() => Promise.resolve(requestProps)),
+      templatePath: '/foo/bar/index.html'
+    };
+    const app = express();
 
-		app.get('/', createHypernovaMiddleware(options));
+    app.get('/', createHypernovaMiddleware(options));
 
-		request(app)
-			.get('/')
-			.expect(200, `<html>${html}</html>`, err => {
-				expect(options.createRequestProps.callCount).toBe(1);
-				expect(options.renderer.render.callCount).toBe(1);
-				expect(options.renderer.render.args[0][0]).toBe(requestProps);
+    request(app)
+      .get('/')
+      .expect(200, `<html>${html}</html>`, err => {
+        expect(options.createRequestProps.callCount).toBe(1);
+        expect(options.renderer.render.callCount).toBe(1);
+        expect(options.renderer.render.args[0][0]).toBe(requestProps);
 
-				done(err);
-			});
-	});
+        done(err);
+      });
+  });
 });
